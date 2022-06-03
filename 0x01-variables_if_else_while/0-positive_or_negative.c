6e998c418134e71f735a791d47329254a1dcd9b4
Chinecherem <chinecheremubawike@gmail.com>
2022-06-03 14:39:54 -0700

    0-positive_or_negative.c git commit -m 0-positive_or_negative.c
    git commit -m 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,7 +14,7 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n < 0)
 {
-printf("%d is negative (less than Zero)\n", n);
+printf("%d is negative\n", n);
 }
 else if (n == 0)
 {
@@ -22,7 +22,7 @@ printf("%d is zero\n", n);
 }
 else
 {
-printf("%d is positive (Greater than Zero)\n", n);
+printf("%d is positive\n", n);
 }
 return (0);
 }
