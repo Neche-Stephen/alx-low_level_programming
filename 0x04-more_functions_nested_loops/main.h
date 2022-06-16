1a46b9707e55b2bf0140eab0e74fa08e25d8859f
Chinecherem <chinecheremubawike@gmail.com>
2022-06-16 02:16:12 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -65,7 +65,26 @@ void print_numbers(void);
 
  */
 
+
+
+/**
+
+ * _putchar - writes the character c to std
+
+ * @c: parameter to compare
+
+ * Return: 1 or 0
+
+ */
+
+int _putchar(char c);
+
+
+
 void print_most_numbers(void);
+
+
+
 
 
 /**
