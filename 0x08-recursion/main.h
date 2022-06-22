2ccdaf8dfa5e375a415afb69e53d85be50b28a5c
Chinecherem <chinecheremubawike@gmail.com>
2022-06-22 14:40:25 -0700

    2-strlen_recursion.c

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -35,5 +35,17 @@ void _puts_recursion(char *s);
 
 void _print_rev_recursion(char *s);
 
+/**
+
+ * _strlen_recursion - gets string length
+
+ * @s: string to count
+
+ * Return: int value
+
+ */
+
+int _strlen_recursion(char *s);
+
 
 #endif
