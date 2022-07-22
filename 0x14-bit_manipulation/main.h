218061a0a9ea20c273d0dd8c9a0a949e16ab4d81
Chinecherem <chinecheremubawike@gmail.com>
2022-07-21 20:08:59 -0700

    corrected main.h

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,11 +1,13 @@
-#ifndef HOLBERTON_H_
+#ifndef MAIN_H_
 
-#define HOLBERTON_H_
+#define MAIN_H_
 
 #include <stdio.h>
 
 #include <stdlib.h>
 
 unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+
 
 #endif
