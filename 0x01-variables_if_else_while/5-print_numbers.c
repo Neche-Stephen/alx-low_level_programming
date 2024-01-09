50dc263bc9436730adee30fa25e7d9acc6d45756
Neche-Stephen <chinecheremubawike@gmail.com>
2024-01-09 10:46:19 +0000

    Solution yo number 5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
 
+/**
+ * main -  Prints numbers 0 - 9
+ * Return:  returns zero
+ *
+ */
+
 int main(void)
 {
-    int i = 0;
+	int i = 0;
 
-    while( i < 10)
-    {
-        printf("%d", i);
-        i++;
-    }
-    printf("\n");
-    return (0);
+	while (i < 10)
+	{
+		printf("%d", i);
+		i++;
+	}
+	printf("\n");
+	return (0);
 }
