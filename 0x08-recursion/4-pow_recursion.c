ba509ffe33af222c2bf587c5f17bc538df66fb60
Chinecherem <chinecheremubawike@gmail.com>
2024-01-11 12:29:44 +0100

    Solution to power

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,23 @@
+include "main.h"
+
+/**
+ * _pow_recursion - calculates the power of a number using recursion
+ * @x: base number
+ * @y: exponent
+ *
+ * Return: The calculated power of 'x' raised to the power of 'y'.
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 1)
+	{
+		return x
+	}
+	--y;
+	return x * _pow_recursion(x, y)
+}
