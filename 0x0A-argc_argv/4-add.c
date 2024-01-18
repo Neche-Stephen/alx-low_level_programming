aed7597dd803ca760f68b8cf259ce82a0e037304
Chinecherem <chinecheremubawike@gmail.com>
2024-01-18 13:29:09 +0100

    Solution to 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - A function that adds positive numbers from the command line
+ * @argc: The number of command-line arguments
+ * @argv: An array containing the command-line arguments
+ *
+ * Return: 0 on successful execution, 1 on error
+*/
+
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+	int count = argc - 1;
+	int i = 1;
+
+	if (argc < 2)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	while(count--)
+	{
+		if (isdigit(*argv[i]))
+		{
+			sum += atoi(argv[i]);
+			i++;
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+		
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
