54cd27b440f05b79dc44d216515d774ba5d2cb0d
Chinecherem <chinecheremubawike@gmail.com>
2024-01-20 12:57:37 +0100

    New solution

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
 * main - tests function that prints if integer is positive or negative
@@ -9,15 +8,9 @@
 int main(void)
 {
         int i;
-	int pn;
 
-        i = 98;
-        pn = positive_or_negative(i);
-
-	if (pn == 0)
-	{
-		printf("0 is zero");
-	}
+        i = 0;
+        positive_or_negative(i);
 
         return (0);
 }
