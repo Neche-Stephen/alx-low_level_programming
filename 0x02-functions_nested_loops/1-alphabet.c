9520e86625162422c00a5b92ab9679aa4df783de
Chinecherem <chinecheremubawike@gmail.com>
2024-01-20 10:33:50 +0100

    New changes

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
 /**
- * main - Print all lower case alphabets
+ * print_alphabet - Print all lower case alphabets
 */
 
 void print_alphabet(void)
 {
 	int i = 97;
+
 	while (i < 123)
 	{
 		_putchar(i);
