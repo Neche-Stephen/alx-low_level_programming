870abc294edc5f9cb2e8ce75be7a8700f9aa42e2
Chinecherem <chinecheremubawike@gmail.com>
2024-01-23 16:31:50 +0100

    All changes

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,17 +1,4 @@
-#include <unistd.h>
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
+#include <stdio.h>
 /**
  * main - prints the name of file it is in
  * Return: returns zero
@@ -20,11 +7,10 @@ int main(void)
 {
 	char *file = __FILE__;
 	int i = 0;
-
-	while (file[i] != '\0')
+	printf("%s", file);
+	while (i != '\0')
 	{
-		_putchar(file[i]);
-		i++;
+		putchar(file[i]);
 	}
 	return (0);
 }
