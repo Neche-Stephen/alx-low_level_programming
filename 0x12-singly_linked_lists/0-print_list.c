da3e9510429e9b382e20735cf845317bedcd9330
Chinecherem <chinecheremubawike@gmail.com>
2024-02-01 14:16:52 +0100

    New changes

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,21 @@
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+size_t print_list(const list_t *h)
+{
+	const list_t *police;
+	int i = 1;
+	police = h;
+
+	while (police->next != NULL)
+	{
+		printf("%d %s\n", police->len, police->str);
+		police = police->next;
+		i++;
+	}
+	printf("%d %s\n", police->len, police->str);
+	return (i);
+
+}
