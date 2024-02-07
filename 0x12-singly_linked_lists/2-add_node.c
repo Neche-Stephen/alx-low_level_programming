d2213adbba8a552c7f4720c8b82703201700f39b
Chinecherem <chinecheremubawike@gmail.com>
2024-02-07 16:55:42 +0100

    New changes

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -16,20 +16,21 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
 	new = malloc(sizeof(list_t));
+	
 	if (new == NULL)
 	{
 		free(new);
-		return NULL;
+		return (NULL);
 	}
 	new->str = strdup(str);
 	if ((new->str) == NULL)
 	{
 		free(new->str);
-		return NULL;
+		return (NULL);
 	}
 	new->len = strlen(new->str);
 
 	new->next = *head;
 	*head = new;
-	return new;
+	return (new);
 }
